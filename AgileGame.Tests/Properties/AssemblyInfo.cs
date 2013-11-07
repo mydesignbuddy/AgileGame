8278e8004908b182994d218a68609452f8d0c673
Buddy Toups <buddyt@blinds.com>
2013-11-07 08:54:06 -0600

    Correct test projects name to singular

--- AgileGame.Tests/Properties/AssemblyInfo.cs
+++ AgileGame.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgileGames.Tests")]
+[assembly: AssemblyTitle("AgileGame.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgileGames.Tests")]
+[assembly: AssemblyProduct("AgileGame.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
