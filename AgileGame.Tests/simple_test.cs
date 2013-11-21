8b83aca94a240caa876aee735088d0149c446a6d
Buddy Toups <buddyt@blinds.com>
2013-11-21 11:00:54 -0600

    add a simple unit test to make sure teamcity config works

---
+++ AgileGame.Tests/simple_test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AgileGame.Tests
+{
+    [TestFixture]
+    public class simple_test
+    {
+        [Test]
+        public void calc_2_plus_3()
+        {
+            var num1 = 2;
+            var num2 = 3;
+            var results = num1 + num2;
+            results.Should().Be(5);
+        }
+    }
+}
